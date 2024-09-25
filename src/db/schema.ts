3bccf6a533bd96d897e636cccf1c049333afb1c8
iDamjan <damjan.stojanovski123@gmail.com>
2024-09-25 15:09:48 +0200

    Add shadcn

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const bids = pgTable("bids", {
   id: serial("id").primaryKey(),
+  bid: text("bid").notNull(),
 });
